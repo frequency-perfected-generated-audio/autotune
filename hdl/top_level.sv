326450e0b908733849869fdc9f167c87cecfbde8
Shruti Siva <shruti.siva@gmail.com>
2024-12-09 21:55:45 -0500

    less loud

--- hdl/top_level.sv
+++ hdl/top_level.sv
@@ -144,7 +144,7 @@ module top_level (
         .NBITS(32)
     ) audio_generator (
         .clk_in(clk_100mhz),
-        .d_in  (audio),
+        .d_in  (audio[31:1]),
         .rst_in(sys_rst),
         .d_out (spk_out)
     );
